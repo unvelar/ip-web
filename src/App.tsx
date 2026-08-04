0605b2f57bd2a4e99bbed52d8ec1dd0c85d7dbe5
bloodybit <riccardo.sibani@gmail.com>
2026-08-04 15:12:35 +0200

    Add shareable product task routes

--- src/App.tsx
+++ src/App.tsx
@@ -127,6 +127,7 @@ export default function App() {
           <Route path="/monitoring/campaigns" element={<MonitoringCampaigns />} />
           <Route path="/monitoring/campaigns/:campaignId" element={<MonitoringCampaigns />} />
           <Route path="/monitoring/products" element={<ProductClusters />} />
+          <Route path="/monitoring/products/:groupId/tasks/:taskId" element={<ProductClusters />} />
           <Route path="/monitoring/new" element={<MonitoringNew />} />
           <Route path="/monitoring/settings" element={<Monitors />} />
           <Route path="/clearance/tasks" element={<ClearanceTasks />} />
