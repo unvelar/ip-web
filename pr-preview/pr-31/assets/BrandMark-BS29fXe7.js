2ae57d2db2d0afe479059b9ffca40e00ff289caa
bloodybit <riccardo.sibani@gmail.com>
2026-09-16 10:32:34 +0000

    Deploy preview for PR 31: 889f97acf157d8ae9a64e5e569fc164b77f9dea1

---
+++ pr-preview/pr-31/assets/BrandMark-BS29fXe7.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-DRFLMprJ.js";var t=e(),n=`/pr-preview/pr-31/logo/logo.svg`;function r({className:e=``}){return(0,t.jsx)(`img`,{className:e,src:n,width:`512`,height:`512`,alt:``,"aria-hidden":`true`,decoding:`async`,draggable:!1})}export{r as t};
