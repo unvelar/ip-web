a339e6d66714803adb0861fd51cf4b62b3afe65c
bloodybit <bloodybit@users.noreply.github.com>
2026-09-16 10:34:25 +0000

    Remove preview for PR 31

--- pr-preview/pr-31/assets/publicSummary-DOKHfq2H.js
+++
@@ -1,1 +0,0 @@
-function e(e){return e.public_summary_enabled!==!0||!e.tenant_public_slug||!e.public_slug?null:`${window.location.origin}/brand-sumups/${e.tenant_public_slug}/${e.public_slug}`}export{e as t};
