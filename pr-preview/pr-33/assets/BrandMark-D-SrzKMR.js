dad98e446e9611f0a23f47fa27b879ccea941e08
bloodybit <riccardo.sibani@gmail.com>
2026-09-18 14:53:42 +0000

    Deploy preview for PR 33: c8dee6155358316342c86fe0aa807ab31b861b97

---
+++ pr-preview/pr-33/assets/BrandMark-D-SrzKMR.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-B3SbtncX.js";var t=e(),n=`/pr-preview/pr-33/logo/logo.svg`;function r({className:e=``}){return(0,t.jsx)(`img`,{className:e,src:n,width:`512`,height:`512`,alt:``,"aria-hidden":`true`,decoding:`async`,draggable:!1})}export{r as t};
---
+++ pr-preview/pr-33/assets/chevron-right-CDvwXZqt.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-exa-rzKI.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
