2ac9c263d4cf77a1945851827306b2709f602e4c
bloodybit <bloodybit@users.noreply.github.com>
2026-09-21 16:26:16 +0000

    deploy: 9ffd43fcef805eb985e7663866b8be027178cf19

---
+++ assets/MonitoringTasks-BeP8D9qM.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-C0uR3_Dm.js";import{MonitoringInboxView as t}from"./Findings-BkuVwCIU.js";var n=e();function r(){return(0,n.jsx)(`div`,{className:`monitoring-task-page`,children:(0,n.jsx)(t,{})})}export{r as default};
