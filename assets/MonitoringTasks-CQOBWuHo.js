e590400a14906f263ebff75a7648a769921aa370
snieking <snieking@users.noreply.github.com>
2026-09-23 13:05:00 +0000

    deploy: db4a4377ef9ace40c313fee958c9be5262748234

---
+++ assets/MonitoringTasks-CQOBWuHo.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-C0uR3_Dm.js";import{MonitoringInboxView as t}from"./Findings-2AafjpFf.js";var n=e();function r(){return(0,n.jsx)(`div`,{className:`monitoring-task-page`,children:(0,n.jsx)(t,{})})}export{r as default};
