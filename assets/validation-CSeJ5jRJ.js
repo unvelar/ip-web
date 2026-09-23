52774a5674c6de0110a261780162d47d78f61c34
snieking <snieking@users.noreply.github.com>
2026-09-23 22:03:48 +0000

    deploy: dd224d53df778493551ab989967d61f9ef19c8bc

---
+++ assets/validation-CSeJ5jRJ.js
@@ -0,0 +1,1 @@
+var e=class extends Error{constructor(e){super(`The server returned an invalid ${e}. Please try again.`),this.name=`InvalidApiResponseError`}};function t(e){return typeof e==`object`&&!!e&&!Array.isArray(e)}function n(t,n){if(!t)throw new e(n)}export{n,t};
